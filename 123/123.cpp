39bbb9035f231bba0d2e9249e4b3d73e1f3d9653
pekkey <46312812+pekkey@users.noreply.github.com>
2019-03-22 11:21:55 +0800

    添加项目文件。

---
+++ 123/123.cpp
@@ -0,0 +1,13 @@
+// 123.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<graphics.h>
+#include<conio.h>
+void main() {
+	initgraph(640, 480);
+	circle(200, 200, 100);
+	_getch();
+	closegraph();
+
+}
